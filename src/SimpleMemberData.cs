5158cc676d4bb3b513ae902dcce306453c267bcc
Amadeusz Wieczorek <git@amadeusw.com>
2017-08-17 10:17:16 -0700

    cleans up output for better diffing

--- src/SimpleMemberData.cs
+++ src/SimpleMemberData.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace AA
@@ -16,7 +17,12 @@ namespace AA
         {
             var data = FromBuffer(buffer);
             var niceKind = data.First().Substring(1, 1).ToUpper() + data.First().Substring(2);
-            var everythingElse = data.Skip(1);
+
+            // Skip these attributes. They are associated with default constructors.
+            if (data.Any(n => n.Contains("DebuggerBrowsableAttribute")))
+                return null;
+
+            var everythingElse = data.Skip(1).Cleanup();
             return new SimpleMemberData
             {
                 Kind = niceKind,
@@ -27,5 +33,17 @@ namespace AA
         static string[] FromBuffer(string buffer) => buffer.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
 
         public override string ToString() => $"{Kind} {Content}";
+    }
+
+    public static class LinqExtensions
+    {
+        static string pattern = @">\S+__\S+'";
+        static Regex r = new Regex(pattern, RegexOptions.IgnoreCase);
+
+        public static IEnumerable<string> Cleanup(this IEnumerable<string> e)
+        {
+            foreach (var n in e)
+                yield return  r.Replace(n, ">___'");
+        }
     }
 }
